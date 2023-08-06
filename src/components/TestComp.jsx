f47a0507fdf381656dfadf4ea768fedad9ad21ef
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-06 16:05:39 -0500

    home story

--- src/components/TestComp.jsx
+++ src/components/TestComp.jsx
@@ -6,18 +6,17 @@ function TestComp() {
         <div id='homePage'>
           <img src="http://www.monsterhunterworld.com/sp/images/common/border_section.png" alt="border" className='border'/>
         <div className="homeHistory">
-          <br />
-          <br />
           <div className="mhStory">
             <p>
-              The world of <span className='mhTitle'>Monster Hunter</span> is filled with vibrant landscapes, 
+              <span style={{marginLeft: '5rem'}}>{' '}</span>The world of <span className='mhTitle'>Monster Hunter</span> is filled with vibrant landscapes, 
               and even more unique creatures called "<span>monsters</span>". These destructive 
               animals often come in conflict with civilization, threatening people that trespass 
               on their territory, make their homes in bad positions, and 
               even raiding villages for food. You play as a "<span>Hunter</span>", a person 
               who has trained their entire life to combat down these creatures when needed, whether 
               for renown or the sake of a greater cause.
-              <br />
+            </p>
+            <p>
               Of course, to better fight your marks, one must learn more about the creatures in question, 
               strengthen your armaments with the spoils of war, and continue honing your hunting skills...
             </p>
--- src/components/TestyComp.jsx
+++ src/components/TestyComp.jsx
@@ -5,9 +5,7 @@ function TestyComp() {
     <div id='testyTest'>
         <img src="http://www.monsterhunterworld.com/sp/images/common/border_section.png" alt="" />
         <div className='testingBox'>
-          <p>
-            The world of Monster Hunter is filled with vibrant landscapes, and even more unique creatures called "monsters".
-          </p>
+          <p> images here</p>
         </div>
 
     </div>
